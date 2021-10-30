81f8185deceda153216a17710c050456ca024d16
Andy Pavia <pavia.andres@gmail.com>
2021-10-30 18:58:59 +0200

    adding percentile func

---
+++ helpers/address.js
@@ -0,0 +1,8 @@
+module.exports = {
+    shortenAddress: function(address){
+        const len = address && address.length || 0;
+        if(len > 0 )
+            return address.substring(0, 5) + '...' + address.substring(len-4, len)
+    }
+
+}
